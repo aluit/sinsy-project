90900d83f4df98338b4061b6cd6a7d8b7216b743
aluit <sandrau@rmit.edu.au>
2020-03-20 22:32:15 +1100

    updated addLyrics.py for syllables
    The original addLyrics did not use the correct syllabic element, since there were not syllable markers retained for input.
    This version uses text that has hyphen+space to separate syllables. Then the output uses "begin", "middle" and "end" syllabic text correctly, while removing the hyphens in the final xml, so that sinsy is more likely to pronounce the words correctly.
    This version will also put in an "extend" element for syllables that end with an underscore, but currently there is no way of indicating how many notes the syllable should be extended to.

--- scripts/addLyrics.py
+++ scripts/addLyrics.py
@@ -5,9 +5,19 @@ import argparse
 import xml.etree.ElementTree as ET
 
 def addLyrics(root, lyrics):
+    # Sandra version, process dashes as syllable markers
+    # Process dashes in isolation as melisma (middle syllables auto extend unlyricked notes)
+    # Also process underscores at the end of a word as melisma
+    # If syllabic tags are correctly applied, and dashes removed, sinsy works better.
+    word = ''
     n = len(lyrics)
+    edlyrics = list( lyrics ) # need a copy, since addLyric loops through text as often as needed
+    #print edlyrics
+    #print lyrics
     i = 0
     for a in root.findall('part'):
+        middle = False
+        extend = False
         for b in a.findall('measure'):
             for c in b.findall('note'):
                 hasRest = False
@@ -30,13 +40,69 @@ def addLyrics(root, lyrics):
                     if not hasSyllabic:
                         syllabicElt = ET.SubElement(d, 'syllabic')
                     for e in d.findall('syllabic'):
-                        e.text = 'single'
+                        word = lyrics[i%n]
+                        lastchar = word[len(word) - 1] 
+                        # print word, word[len(word) - 1] , lastchar
+                        if not middle:
+                            # start of a word
+                            if lastchar != '-' and lastchar != '_':
+                                # doesn't end in dash or underscore, so single
+                                e.text = 'single'
+                            elif lastchar == '-':
+                                # first syllable of a word (assuming it isn't just a dash...)
+                                e.text = 'begin'
+                                middle = True
+                                word = word[ 0:len(word) - 1 ] # remove dash
+                            elif lastchar == '_':
+                                # monosyllabic extended
+                                e.text = 'single'
+                                word = word[ 0:len(word) - 1 ] # remove underscore
+                                extend = True
+                            else:
+                                # how did that happen?!!
+                                print 'Error: how did this happen, with word = ', word, '?!!'
+                        else:
+                            # in middle of a word
+                            if lastchar != '-' and lastchar != '_':
+                                # end of word
+                                e.text = 'end'
+                                middle = False
+                            elif lastchar == '-':
+                                # middle of word
+                                e.text = 'middle'
+                                word = word[ 0:len(word) - 1 ] # remove dash
+                            elif lastchar == '_':
+                                # polysyllabic extended
+                                e.text = 'end'
+                                word = word[ 0:len(word) - 1 ] # remove underscore
+                                extend = True
+                                middle = False
+                            else:
+                                # how did that happen?!!
+                                print 'Error: how did this happen, with word = ', word, '?!!'
                     if not hasLyricText:
                         lyricTextElt = ET.SubElement(d, 'text')
                     for e in d.findall('text'):
-                        e.text = lyrics[i%n]
+                        e.text = word
                         i+=1
-
+                    hasExtend = False
+                    for e in d.findall( 'extend'):
+                        hasExtend = True
+                    if extend and not  hasExtend:
+                        # syllable extends over following notes, indicated by underscore in input
+                        # and we need to create an extend element
+                        extendElt = ET.SubElement( d, 'extend' )
+                        extend = False
+                    elif not extend and hasExtend:
+                        # uh oh, we need to get rid of an element. How do I do that?
+                        d.remove( extendElt )
+                    else:
+                        # either there is an extend already and we're extending
+                        # or there isn't an extend and we're not extending
+                        # so do nothing
+                        continue
+                
+                            
 
 #
 # Parse command line arguments
